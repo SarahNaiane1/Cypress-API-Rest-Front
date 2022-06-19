13c6702521185f7e54aa599087d67f5278bcde70
sarahnaianes <sarah.santos@mimo.com.br>
2022-06-19 14:24:25 -0300

    add iframe

--- cypress/integration/header.spec.js
+++ cypress/integration/header.spec.js
@@ -35,10 +35,10 @@ describe("register", () => {
   it("invoke", () => {
     cy.window().invoke("alert", "Dá para ver?");
   });
-  it.only("alert", () => {
+  it("alert", () => {
     cy.get("#alert").click();
     cy.on("window:alert", (msg) => {
-      console.log(msg); 
+      console.log(msg);
       expect(msg).to.be.equal("Alert Simples");
     });
   });
@@ -116,7 +116,48 @@ describe("register", () => {
   });
 
   it("work with iframe", () => {
+    cy.get("#frame1").then((iframe) => {
+      const body = iframe.contents().find("body");
+      cy.wrap(body)
+        .find("#tfield")
+        .type("funciona?")
+        .should("have.value", "funciona?");
 
-});
-});
+      cy.visit("https://wcaquino.me/cypress/frame.html  ");
+      cy.get("#otherButton").click();
+      cy.on("window:alert", (msg) => {
+        expect(msg).to.be.equal("Click OK!");
+      });
+    });
+  });
 
+  it("work with popup", () => {
+    cy.window().then((win) => {
+      cy.stub(win, "open").as("winOpen");
+    });
+    cy.get("#buttonPopUp").click();
+    cy.get("@winOpen").should("be.called");
+  });
+
+  describe.only("work with popup ", () => {
+    beforeEach(() => {
+      cy.visit("https://wcaquino.me/cypress/componentes.html");
+    });
+    it("check popup url", () => {
+      cy.contains("Popup2")
+        .should("have.prop", "href")
+        .and("equal", "https://wcaquino.me/cypress/frame.html");
+    });
+    it("popup dinamically", () => {
+      cy.contains("Popup2").then(($a) => {
+        const href = $a.prop("href");
+        cy.visit(href);
+        cy.get("#tfield").type("funciona");
+      });
+    });
+    it("should force link on same page", () => {
+      cy.contains("Popup2").invoke("removeAttr", "target").click();
+      cy.get("#tfield").type("funciona");
+    });
+  });
+});
